7e0f41770d5db6cd34455a6e5712fa7cdb58fa71
Renan Traba <hellupline@gmail.com>
2020-01-10 01:37:09 -0300

    first commit

---
+++ en.search.min.d94ef758a0c2d4a996544df0648fcd1a21ded1bd776e7fc4c672d0e280efc62e.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.1500cabcdee10bd60390b3e010d0421a08e17f1dcdee4eb53e755823a418dc56.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
