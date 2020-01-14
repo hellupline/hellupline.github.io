3958cfb9639203109984ee396ef850bfc3d4ebe7
hellupline <hellupline@users.noreply.github.com>
2020-01-14 04:30:47 +0000

    Automated deployment: Tue Jan 14 04:30:47 UTC 2020 hellupline/hellupline.github.io-source@0b391197a72723da4a0f6e9905205fd71534ad56

--- en.search.min.9e95f7dec4381980cc283517eac3bc5acfc19c39e11da009f8c101e1d866623a.js
+++
@@ -1,5 +0,0 @@
-'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.6dacc37c38bcf2ddbf91d158e6a5d90f556746b22d54f760150c79b60b64c52f.js',function(){input.required=false;search();});}
-function search(){while(results.firstChild){results.removeChild(results.firstChild);}
-if(!input.value){return;}
-const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
-function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
