c396eab72d31db4222f3842bc877a028ad086e6a
hellupline <hellupline@users.noreply.github.com>
2022-11-14 13:45:04 +0000

    Automated deployment: Mon Nov 14 13:45:04 UTC 2022 hellupline/hellupline.github.io-source@948e0a5d2f7c296aa030e976cd02d43ffd0e40ee

--- files/docs/scripts/python/asyncio-queue-requests.py
+++ files/docs/scripts/python/asyncio-queue-requests.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# poetry add httpx=='^0.23.0' pyquery=='^1.4.3'
+
 import logging
 
 from asyncio import Event
@@ -12,6 +14,7 @@ from functools import partial
 from typing import Any
 from typing import Callable
 from typing import Coroutine
+from urllib.parse import urlparse
 
 from httpx import AsyncClient
 from pyquery import PyQuery
@@ -83,18 +86,18 @@ async def queue_task(
     task_index: int,
     function: Callable[..., Coroutine[Any, Any, Any]],
 ):
-    logger.debug(f"{function.__name__}({task_index}) started")
+    logger.debug("%s(%d) started", function.__name__, task_index)
     while True:  # not queue_input.empty()
         try:
             data = await queue_input.get()
         except CancelledError:
-            logger.debug(f"{function.__name__}({task_index}) cancelled")
+            logger.debug("%s(%d) cancelled", function.__name__, task_index)
             return
-        logger.debug(f"{function.__name__}({task_index}) extracting urls")
+        logger.debug("%s(%d) extracting urls", function.__name__, task_index)
         try:
             await function(data=data)
         except Exception:  # pylint: disable=broad-except
-            logger.exception(f"{function.__name__}({task_index}) failed to run task")  # raise
+            logger.exception("%s(%d) failed to run task", function.__name__, task_index)  # raise
         finally:
             queue_input.task_done()
 
@@ -117,11 +120,7 @@ async def _extract_data(
 ):
     text, base_url = data
     q = PyQuery(text).make_links_absolute(base_url=base_url)
-    urls = {
-        url
-        for url in (tag.attrib.get("href", "") for tag in q("a"))
-        if url.startswith(base_url) and url.endswith("/")
-    } - visited
+    urls = {u for u in (t.attrib.get("href", "") for t in q("a")) if _valid_url(u, base_url)} - visited
     title = q("title").text().strip()
     visited.update(urls)
     for url in urls:
@@ -129,6 +128,12 @@ async def _extract_data(
     if not urls:
         event_completed.set()
     await queue_output_2.put(title)
+
+
+def _valid_url(url: str, base_url: str) -> bool:
+    base_u = urlparse(base_url)
+    u = urlparse(url)
+    return base_u.hostname == u.hostname and u.path.endswith("/")
 
 
 async def _print_title(data: str):
