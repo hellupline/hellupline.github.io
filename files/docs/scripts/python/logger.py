43fc02337f58340dc8f30e7d1ba6ffe7f927f65e
hellupline <hellupline@users.noreply.github.com>
2023-05-02 19:05:16 +0000

    deploy: hellupline/hellupline.github.io-source@7a114b98eacc12b7577e56c2860b96691e67da55

--- files/docs/scripts/python/logger.py
+++ files/docs/scripts/python/logger.py
@@ -1,9 +1,22 @@
 #! /usr/bin/env python3
 
 import logging
+import pathlib
 import sys
 
-log_format = "[%(asctime)s : %(levelname)s : %(pathname)s : %(lineno)s : %(funcName)s] %(message)s"
-logging.basicConfig(format=log_format, datefmt="%Y-%m-%dT%H:%M:%S%z", stream=sys.stdout)
+logs_path = pathlib.Path("logs")
+logs_path.mkdir(parents=True, exist_ok=True)
+
+# log_format = "[%(asctime)s : %(levelname)s : %(name)s : %(pathname)s : %(lineno)s : %(funcName)s] %(message)s"
+log_format = "[%(asctime)s:%(levelname)s] %(message)s"
+log_datefmt = "%Y-%m-%dT%H:%M:%S%z"
+log_filename = logs_path / "{:%Y-%m-%dT%H:%M:%S}.log".format(datetime.now())
+logging.basicConfig(
+    format=log_format,
+    datefmt=log_datefmt,
+    filename=log_filename,
+    # stream=sys.stdout,
+)
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
